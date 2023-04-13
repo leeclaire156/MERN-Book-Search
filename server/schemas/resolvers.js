5b5edc85ea7edefd46015cf7b23d69e79ec4991e
Claire Lee <lee.claire156@gmail.com>
2023-04-13 18:11:42 -0400

    Adds resolver with query and mutation functions

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,71 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('Please log in.');
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect email or password.');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect email or password.');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        // Add a third argument to the resolver (context) to access data in our `context`
+        // This makes it so a logged in user can only remove a book from their own profile
+        saveBook: async (parent, { }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: userId },
+                    {
+                        $addToSet: { books: book },
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                )
+            }
+            // If user tries to perform this mutation & isn't logged in, throw this error
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async (parent, { book }) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { books: book } },
+                    { new: true }
+                );
+            } throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -34,7 +34,7 @@ type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
 
-    savedBook(input: Book): User
+    saveBook(input: Book): User
     removeBook(bookId: String!): User
 }
 `
